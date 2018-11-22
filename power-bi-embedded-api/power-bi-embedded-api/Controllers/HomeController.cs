bd7bc7a70dfdc69a02dfcff2c876817564930e73
Krappie <auretswanepoel@rocketmail.com>
2018-11-22 21:38:07 +1100

    added authenticate and create-credential methods

--- power-bi-embedded-api/power-bi-embedded-api/Controllers/HomeController.cs
+++ power-bi-embedded-api/power-bi-embedded-api/Controllers/HomeController.cs
@@ -1,13 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Microsoft.Rest;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace power_bi_embedded_api.Controllers
 {
     public class HomeController : Controller
     {
+        private async Task<AuthenticationResult> Authenticate()
+        {
+            // Create a user password credentials.
+            var credential = new UserPasswordCredential("Username", "Password");
+            // Authenticate using created credentials
+            var authenticationContext = new AuthenticationContext("AuthorityUrl");
+            var authenticationResult =
+               await authenticationContext.AcquireTokenAsync("ResourceUrl", "ApplicationId", credential);
+            return authenticationResult;
+        }
+        private async Task<TokenCredentials> CreateCredentials()
+        {
+            AuthenticationResult authenticationResult = await Authenticate();
+            if (authenticationResult == null)
+            {
+                return null;
+            }
+            TokenCredentials tokenCredentials = new TokenCredentials(authenticationResult.AccessToken, "Bearer");
+            return tokenCredentials;
+        }
+
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
