a9d2c1ac0993a2aac9ba5b0e1a96f6183ae67dd7
Krappie <auretswanepoel@rocketmail.com>
2018-11-28 21:38:59 +1100

    added embed config model

---
+++ power-bi-embedded-api/power-bi-embedded-api/Models/EmbedConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.PowerBI.Api.V2.Models;
+namespace power_bi_embedded_api.Models
+{
+    [Serializable]
+    public class EmbedConfig
+    {
+        public string Id { get; set; }
+
+        public string EmbedUrl { get; set; }
+
+        public EmbedToken EmbedToken { get; set; }
+
+        public string ErrorMessage { get; internal set; }
+    }
+}
