c1ae8851acea3bc5343a32a7b33eec7d23c3c541
Krappie <auretswanepoel@rocketmail.com>
2018-12-03 19:57:35 +1100

    fixed key to be application ID

--- Registering you Azure App.ps1
+++ Registering you Azure App.ps1
@@ -1,10 +1,6 @@
-$Credentials = Get-Credential 
-Connect-AzureAD -Credential $Credentials 
-
 $appName = "PowerShell Test App"
 $appURI = "http://localhost:4200"
 
-if(!($myApp = Get-AzureADApplication -Filter "DisplayName eq '$($appName)'" -ErrorAction SilentlyContinue))
-{
-    $myApp = New Get-AzureADApplication -DisplayName $appName -IdentifierUris $appURI
-}
+$myApp = Get-AzureADApplication -Filter "DisplayName eq '$($appName)'"
+
+$myApp.appId
