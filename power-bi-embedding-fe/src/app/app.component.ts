37e67c530bb6832d9ba885503114fffb61486948
Krappie <auretswanepoel@rocketmail.com>
2018-12-03 23:04:24 +1100

    added getReports method FE

--- power-bi-embedding-fe/src/app/app.component.ts
+++ power-bi-embedding-fe/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit  } from '@angular/core';
+import { PowerbiService } from './services/powerbi.service';
+import { Report } from './models/report.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'power-bi-embedding-fe';
+  myReports: Report[] = [];
+  constructor(private powerbiService: PowerbiService) {}
+  ngOnInit() {
+    this.loadAllReports();
+  }
+  loadAllReports() {
+    this.powerbiService.getReports().subscribe(reports => {
+       this.myReports = reports;
+       console.log(this.myReports);
+    });
+  }
 }
