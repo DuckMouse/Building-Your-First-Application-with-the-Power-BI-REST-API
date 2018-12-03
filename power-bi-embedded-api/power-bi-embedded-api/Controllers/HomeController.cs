2b30d2c729515c5d0be16ae2c65c312e526ff5dc
Krappie <auretswanepoel@rocketmail.com>
2018-12-03 21:46:39 +1100

    added embed reports method

--- power-bi-embedded-api/power-bi-embedded-api/Controllers/HomeController.cs
+++ power-bi-embedded-api/power-bi-embedded-api/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Web.Mvc;
 using power_bi_embedded_api.Models;
 using System;
 using Microsoft.PowerBI.Api.V2;
+using Microsoft.PowerBI.Api.V2.Models;
 
 namespace power_bi_embedded_api.Controllers
 {
@@ -62,6 +63,58 @@ namespace power_bi_embedded_api.Controllers
                 return Json(ex.Message, JsonRequestBehavior.AllowGet);
             }
         }
+        public async Task<ActionResult> EmbedReport(string reportId, string workspaceId)
+        {
 
+            if (string.IsNullOrWhiteSpace(reportId))
+            {
+                reportId = _powerbiSettings.ReportId;
+            }
+
+            if (string.IsNullOrWhiteSpace(workspaceId))
+            {
+                workspaceId = _powerbiSettings.WorkspaceId;
+            }
+            var result = new EmbedConfig();
+            try
+            {
+                TokenCredentials tokenCredentials = await CreateCredentials();
+                if (tokenCredentials == null)
+                {
+                    result.ErrorMessage = "Authentication Failed.";
+                    return Json(result, JsonRequestBehavior.AllowGet);
+                }
+                using (var client = new PowerBIClient(new Uri(_powerbiSettings.ApiUrl), tokenCredentials))
+                {
+
+                    GenerateTokenRequest generateTokenRequestParameters = new GenerateTokenRequest(accessLevel: "view");
+
+                    Report report = await client.Reports.GetReportInGroupAsync(workspaceId, reportId);
+
+                    var tokenResponse = await client.Reports.GenerateTokenInGroupAsync(workspaceId, report.Id, generateTokenRequestParameters);
+
+                    if (tokenResponse == null)
+                    {
+                        result.ErrorMessage = "Failed to generate embed token.";
+
+                        return Json(result, JsonRequestBehavior.AllowGet);
+                    }
+
+                    result.EmbedToken = tokenResponse;
+                    result.EmbedUrl = report.EmbedUrl;
+                    result.Id = report.Id;
+
+                    return Json(result, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                result.ErrorMessage = ex.Message;
+
+                return Json(result, JsonRequestBehavior.AllowGet);
+
+            }
+        }
     }
 }
